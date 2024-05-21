aa732bf3c50dbef22fa822d09ac60a80574e3ec3
lucasleos <lucasleosuarez10@gmail.com>
2024-05-21 17:19:44 -0300

    Renombre de Inicio a EncuestaList

--- app/src/main/java/unpsjb/ing/tntpm2024/listado/EncuestaListFragment.kt
+++ app/src/main/java/unpsjb/ing/tntpm2024/listado/EncuestaListFragment.kt
@@ -1,38 +1,33 @@
-package unpsjb.ing.tntpm2024.inicio
+package unpsjb.ing.tntpm2024.listado
 
 import android.os.Build
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import androidx.databinding.DataBindingUtil
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import unpsjb.ing.tnt.ligadeportiva.listado.listado.EncuestaListAdapter
 import unpsjb.ing.tntpm2024.R
 import unpsjb.ing.tntpm2024.basededatos.Encuesta
 import unpsjb.ing.tntpm2024.databinding.FragmentInicioBinding
 import unpsjb.ing.tntpm2024.encuesta.EncuestaViewModel
+import unpsjb.ing.tntpm2024.inicio.InicioViewModel
 
-class InicioFragment : Fragment() {
+class EncuestaListFragment : Fragment() {
 
-    val TAG = "InicioFragment"
+    val TAG = "EncuestaListFragment"
 
-    //private lateinit var adapterList :  EncuestaListAdapter
-    private val adapterList : EncuestaListAdapter by lazy {EncuestaListAdapter(requireContext())}
+    private val adapterList: EncuestaListAdapter by lazy { EncuestaListAdapter(requireContext()) }
 
     private val viewModel: InicioViewModel by viewModels()
     private lateinit var encuestaViewModel: EncuestaViewModel
@@ -81,16 +76,15 @@ class InicioFragment : Fragment() {
         encuestaViewModel.todasLasEncuestas
             .observe(
                 viewLifecycleOwner,
-                Observer {
-                        encuestas ->
-                    encuestas?.let{ adapterList.setEncuestas(it) }
+                Observer { encuestas ->
+                    encuestas?.let { adapterList.setEncuestas(it) }
                 }
             )
 
         val fab = binding.botonFlotante
 
         fab.setOnClickListener {
-            findNavController().navigate(InicioFragmentDirections.actionInicioFragmentToEncuestaFragment())
+            findNavController().navigate(EncuestaListFragmentDirections.actionEncuestalistToEncuestaFragment())
         }
 
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -99,14 +93,16 @@ class InicioFragment : Fragment() {
         adapterList.onItemClick = {
             var asd = it.encuestaCompletada
             //Log.d(TAG, "el alimento es ${it.alimento}")
-            findNavController().navigate(InicioFragmentDirections.actionInicioFragmentToDetailFragment(
-                  //title = it.dataTitle,
-                  //desc = it.dataDesc
-                title = "Detalle Encuesta",
-                desc = "Usted consume ${it.porcion} de ${it.alimento}, ${it.veces} cada ${it.frecuencia} \n" +
-                        //"estado: ${it.encuestaCompletada}")
+            findNavController().navigate(
+                EncuestaListFragmentDirections.actionEncuestalistToDetailFragment(
+                    //title = it.dataTitle,
+                    //desc = it.dataDesc
+                    title = "Detalle Encuesta",
+                    desc = "Usted consume ${it.porcion} de ${it.alimento}, ${it.veces} cada ${it.frecuencia} \n" +
+                            //"estado: ${it.encuestaCompletada}")
 
-                        "Estado: " + if (it.encuestaCompletada) "Completada" else "Incompleta")
+                            "Estado: " + if (it.encuestaCompletada) "Completada" else "Incompleta"
+                )
             )
         }
 
